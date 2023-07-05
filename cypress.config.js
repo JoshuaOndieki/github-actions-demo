2c2e7748b71fd1de7233a58640c0f03a329a88fa
Joshua Ondieki <oj@joshuaondieki.com>
2023-07-05 21:14:40 +0300

    demo site with simple cypress title test

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "ad8t48",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ src/scripts/index.ts
@@ -0,0 +1,4 @@
+function sayHi() {
+    const pElement = document.getElementById('hi') as HTMLParagraphElement
+    pElement.style.display = 'block'
+}
